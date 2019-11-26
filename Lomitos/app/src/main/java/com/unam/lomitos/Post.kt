26b2a9bd8ef9906d6091fcd3d05f6c69472b354d
JuanLugoN <juanlugo@ciencias.unam.mx>
2019-11-26 13:41:31 -0600

    Adaptador semicompleto

---
+++ Lomitos/app/src/main/java/com/unam/lomitos/Post.kt
@@ -0,0 +1,6 @@
+package com.unam.lomitos
+
+/**
+ * Esta contiene los elementos de una publicación del feed.
+ */
+class Post(val id:Int, val nombre:String, val imagen:String, var likes:Int)
--- Lomitos/app/src/main/java/com/unam/lomitos/feed.kt
+++ Lomitos/app/src/main/java/com/unam/lomitos/feed.kt
@@ -1,7 +1,7 @@
 package com.unam.lomitos
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class feed : AppCompatActivity() {
 
