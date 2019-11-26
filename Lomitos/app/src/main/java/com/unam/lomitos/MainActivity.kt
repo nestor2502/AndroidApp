cd5aa7f745250e870cc3aeb4de6820c54cff8c74
Nestor <nestor2502@ciencias.unam.mx>
2019-11-26 07:43:08 -0600

    cambios en diseño y duncionalidad de actividad login

--- Lomitos/app/src/main/java/com/unam/lomitos/MainActivity.kt
+++ Lomitos/app/src/main/java/com/unam/lomitos/MainActivity.kt
@@ -1,14 +1,27 @@
 package com.unam.lomitos
 
 import android.content.Intent
+import android.os.AsyncTask
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
 import android.view.View
 import android.widget.EditText
+import android.widget.ProgressBar
 
+//import para api
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import org.jetbrains.anko.alert
+import retrofit2.Response
+import android.util.Log
+import android.widget.Button
+import retrofit2.Call
+import retrofit2.Callback
+import com.google.gson.Gson
+import org.jetbrains.anko.activityUiThread
+import org.jetbrains.anko.doAsync
+import org.jetbrains.anko.uiThread
 
 
 class MainActivity : AppCompatActivity() {
@@ -16,6 +29,9 @@ class MainActivity : AppCompatActivity() {
     lateinit var service: ApiService
     private lateinit var editText:EditText
     private lateinit var editText2:EditText
+    private lateinit var progressBar: ProgressBar
+    private lateinit var button: Button
+    val TAG_LOGS = "nestor"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,6 +39,16 @@ class MainActivity : AppCompatActivity() {
 
         editText = findViewById(R.id.editText)
         editText2 = findViewById(R.id.editText2)
+        progressBar = findViewById(R.id.progressBar)
+        button = findViewById(R.id.button)
+
+        val retrofit: Retrofit = Retrofit.Builder()
+            .baseUrl("http://lomitos-api.tk/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+        service = retrofit.create<ApiService>(ApiService::class.java)
+
 
     }
 
@@ -31,11 +57,29 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun login(view:View){//accion del boton login
+
         //loginUser()
         val username:String = editText.text.toString() //giving the username of text view
         val password:String = editText2.text.toString() //giving the password of text view
-        conn(username, password)
-        print("Hola")
+
+        if(!TextUtils.isEmpty(username)&&!TextUtils.isEmpty(password)){
+            //progressBar.visibility= View.VISIBLE
+            //getKey()
+            progressBar.visibility= View.VISIBLE
+            doAsync {
+                button.isClickable = false
+                Thread.sleep(5000)
+                uiThread {
+                    progressBar.visibility=View.GONE
+                    button.isClickable = true
+                }
+            }
+
+        }
+        else{
+            showErrorDialogEmptyField()
+        }
+
     }
 
     private fun action(){
@@ -45,32 +89,29 @@ class MainActivity : AppCompatActivity() {
         startActivity(Intent(this, connected::class.java))
     }
 
-    fun loginUser(){
+    fun getKey(){
+
         val username:String = editText.text.toString() //giving the username of text view
         val password:String = editText2.text.toString() //giving the password of text view
-        action2()
-        /**
-        doAsync {
-            val call = getRetrofit().create(ApiService::class.java).getKey(username, password).execute()
-            val puppies = call.body() as User
-            uiThread {
-            if (puppies.status == "success") {
+
+        var post: User? = null
+
+        service.getKey(username, password).enqueue(object: Callback<User>{
+            override fun onResponse(call: Call<User>?, response: Response<User>?) {
+                //post = response?.body()
+                //Log.i(TAG_LOGS, Gson().toJson(post))
+                //print(post.toString())
                 action2()
-
-            } else {
-                showErrorDialog()
             }
-                hideKeyboard()}
-        }*/
+
+            override fun onFailure(call: Call<User>, t: Throwable) {
+                //TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+                t?.printStackTrace()
+            }
+        })
+
     }
 
-    private fun getRetrofit(): Retrofit {
-        return Retrofit.Builder()
-            .baseUrl("https://dog.ceo/api/breed/")
-            .addConverterFactory(GsonConverterFactory.create())
-            .build()
-        //service = retrofit.create<ApiService>(ApiService::class.java)
-    }
     private fun showErrorDialog() {
         alert("Ha ocurrido un error, inténtelo de nuevo.") {
             yesButton { }
@@ -86,6 +127,14 @@ class MainActivity : AppCompatActivity() {
         val imm = getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
         imm.hideSoftInputFromWindow(viewRoot.windowToken, 0)
     }*/
+
+    private fun showErrorDialogEmptyField() {
+        alert("No puedes dejar campos vacios") {
+            yesButton { }
+        }.show()
+    }
+
+
 
 
 }
--- Lomitos/app/src/main/java/com/unam/lomitos/User.kt
+++ Lomitos/app/src/main/java/com/unam/lomitos/User.kt
@@ -1,3 +1,3 @@
 package com.unam.lomitos
 
-class User (var status :String, var key: String)
+data class User (var status :String, var key: String)
