cf09a35643ed11e8aae36cee0c1c397463002867
JoseRamirezRojas <josedavidrr@ciencias.unam.mx>
2019-12-04 23:55:04 -0600

    LIke finalizado

--- Lomitos/app/src/main/java/com/unam/lomitos/AdaptadorPost.kt
+++ Lomitos/app/src/main/java/com/unam/lomitos/AdaptadorPost.kt
@@ -8,6 +8,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageButton
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import androidx.core.content.ContextCompat.startActivity
@@ -24,12 +25,12 @@ class AdaptadorPost(var posts: JSONArray, val context: Context, val key:String):
     //val onItemClickListener: AdapterView.OnItemClickListener? = null
 
     override fun onBindViewHolder(holder:AdaptadorPost.ViewHolder, position: Int) {
-        holder.bind_Items(post=posts.getJSONObject(position))
+        holder.bind_Items(post=posts.getJSONObject(position),key = key)
 
         holder.paw?.setOnClickListener(View.OnClickListener {
             holder.like(key)
             holder.refresh_likes(key)
-            holder.refresh_paw()
+            holder.refresh_paw(key)
         })
 
         holder.comment?.setOnClickListener(View.OnClickListener {
@@ -80,7 +81,7 @@ class AdaptadorPost(var posts: JSONArray, val context: Context, val key:String):
         var more: ImageButton? = null
 
         @SuppressLint("WrongViewCast")
-        fun bind_Items(post: JSONObject) {
+        fun bind_Items(post: JSONObject, key: String) {
             val nombre = view.findViewById(R.id.nombre) as TextView
             val likes = view.findViewById(R.id.likes) as TextView
             val imagen = view.findViewById(R.id.foto) as NetworkImageView
@@ -92,6 +93,8 @@ class AdaptadorPost(var posts: JSONArray, val context: Context, val key:String):
             likes.text = post.getString("Likes")
             id.text = post.getString("Id")
             imagen.setImageUrl(post.getString("Imagen"),VolleyService.imageLoader)
+            refresh_likes(key)
+            refresh_paw(key)
         }
 
         fun like(key: String) {
@@ -105,8 +108,20 @@ class AdaptadorPost(var posts: JSONArray, val context: Context, val key:String):
             VolleyService.requestQueue.add(request)
         }
 
-        fun refresh_paw() {
-
+        fun refresh_paw(key: String) {
+            val url = "http://lomitos-api.tk/iflike.php?key=%s&id=%s"
+            val paw = itemView.findViewById<ImageView>(R.id.paw)
+            val request = JsonObjectRequest(Request.Method.GET, url, null,
+                Response.Listener<JSONObject> { response ->
+                    if (response["status"].toString() == "true")
+                        paw.setImageResource(R.drawable.paw_like)
+                    else
+                        paw.setImageResource(R.drawable.paw)
+                },
+                Response.ErrorListener {
+                    Toast.makeText(itemView.context, "That didn't work!", Toast.LENGTH_SHORT).show()
+                })
+            VolleyService.requestQueue.add(request)
         }
 
         fun refresh_likes(key: String) {
